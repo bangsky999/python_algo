6ad983d0fde80429730a0ad6b79bc53cc35f7ba7
Seungjae Bang <bangsky999@naver.com>
2025-07-21 17:34:48 +0900

    1204 완료

--- 2025.07.21/swea_1204.py
+++ 2025.07.21/swea_1204.py
@@ -1,17 +1,28 @@
-# 테스트 케이스 개수
 T = int(input())
 
 for _ in range(T):
-    case_num = int(input())  # 케이스 번호
-    scores = list(map(int, input().split()))  # 점수 리스트
+    num_test = int(input())
+    test_case = list(map(int, input().split()))
 
-    count = {i: 0 for i in range(101)}  # 0~100 점수 딕셔너리 초기화
+    #count 끝~
+    count = {}
 
-    for score in scores:
-        count[score] += 1  # 점수 등장 횟수 세기
+    for i in test_case:
+        if i not in count:
+            count[i] = 1
+        else:
+            count[i] += 1
 
-    max_count = max(count.values())  # 가장 많이 나온 횟수
-    most_common = [score for score, cnt in count.items() if cnt == max_count]
-    answer = max(most_common)  # 가장 많이 나온 점수 중 가장 큰 것
+    # key, value 저장 변수 만들기
+    max_count = 0
+    answer = 0
 
-    print(f"#{case_num} {answer}")
+    for key in count:
+        if count[key] > max_count:
+            max_count = count[key]
+            answer = key
+        elif count[key] == max_count and key > answer:
+            answer = key
+
+    print(f"#{num_test} {answer}")
+
