4bac7f37bb7f531ec6c4a9a638e8b80255a71340
unknown <bangsky999>
2025-08-12 22:28:16 +0900

    home 0812

--- 2025.08.11/swea_1215.py
+++ 2025.08.11/swea_1215.py
@@ -1,1 +1,43 @@
-##############회문 미완
+import sys; sys.stdin = open("input.txt")
+
+def pal(txt): # 회문검사
+    val = 0
+    for i in range(len(txt)):
+        check1 = txt[i]
+        check2 = txt[-i-1]
+        if check1 == check2:
+            val = 1
+        else: 
+            val = 0
+            break
+            
+    if val == 1:
+        return txt
+        
+for tc in range(1, 11):
+    N = int(input())
+    arr = [input() for _ in range(8)]
+    cnt = 0
+    # 가로검사
+    for r in range(8):
+        for c in range(8-N+1):
+            slice = arr[r][c:c+N]
+            if slice == pal(slice):
+                cnt += 1
+    # 세로검사 : 세로 슬라이싱 주의! 안됨 ! 문자열로 합할것
+    for r in range(8-N+1):
+        for c in range(8):
+            slice = ''
+            for k in range(N):
+                slice += arr[r+k][c]
+            if slice == pal(slice):
+                cnt += 1
+    '''
+    # 이거 어떡하지?????
+    # for c in range(8):
+    #     for r in range(8 - N + 1):
+    #         slice = arr[c][r:r+N]
+    '''
+            
+
+    print(f'#{tc} {cnt}')
--- 2025.08.11/swea_3143.py
+++ 2025.08.11/swea_3143.py
@@ -1,18 +1,21 @@
+'''
+s: 본문, p: 패턴
+1. in과 슬라이싱을 이용하자, 앞글자만 남기고 len으로 개수 구하기
+2. 
+'''
 import sys; sys.stdin = open("input.txt")
 
 T = int(input())
 for tc in range(1, T+1):
-    t, p = map(str, input().split()) # t는 본문, p는 패턴
-    cnt = 0
-    for i in range(0, len(t) - len(p) + 1):
-        if t[i:i+len(p)] == p:
+    s, p = input().split() # s: banana, p: bana
+    cnt = 0 # p가 몇번 나왔니?
+    i = 0
+    while i <= len(s)-len(p): # p의 길이만큼만 빼고 i를 돌리겠다
+        if s[i:i+len(p)] == p:
             cnt += 1
             i += len(p)
         else:
             i += 1
-    res = len(t) - len(p)*cnt + cnt
-    print(f'#{tc} {res}')
+    ans = len(s)-(len(p)*cnt) + cnt
 
-
-
-    ################ 미완
+    print(f'#{tc} {ans}')
