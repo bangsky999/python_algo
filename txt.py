15fbd59de954aaea680f91d93eefab88231e94ec
unknown <bangsky999>
2025-08-19 22:34:15 +0900

    txt 수정

--- txt.py
+++ txt.py
@@ -1,15 +1,25 @@
-arr = [3,6,7,1,5,4]
-n = len(arr) # n: 원소의 개수
+arr = [1,2,3,4,5]
 
-for i in range(1<<n): # 1<<n : 부분집합의 개수
-    for j in range(n): # 원소의 수만큼 비트를 비교함
-        if i & (1<<j): # i의 j번 비트가 1인 경우
-            print(arr[j], end = ',') # j번 원소 출력
-    print()
-print()
+# 부분집합
+# - 각 원소를 포함할 것인가? 포함하지 않을 것인가?
+# - 비트마스킹(0 or 1)
 
+N = len(arr)
+out = [False]*N # 출력배열(해당 위치 포함 여부)
 
+def f(idx):
+    # 기저조건
+    if idx == N: 
+        for i in range(N):
+            if out[i]:
+                print(arr[i], end = ' ')
+        print()
+        return
+    
+    # 유도조건 (N-1)까지는 뽑을지 말지 결정가능.
+    out[idx] = True # idx번째 부분집합에 포함시키고 
+    f(idx+1) # idx+1 번째 결정하러 가기
+    out[idx] = False # idx번쨰를 포함시키지 말고
+    f(idx+1) # idx+1 번째 결정하러 가기
 
-# 십진수 이진수로 변환
-# bin 내장함수 사용
-print(bin(8)) # 0b1000
+f(0)
